9bfce5e9737890dc75c41a043ab7c1a006a8469d
Dominic Endrei <dominic.endrei@gmail.com>
2022-04-07 16:49:45 +1000

    Fix

--- examples/Main.elm
+++ examples/Main.elm
@@ -93,10 +93,10 @@ update msg model =
                 , awsRegion = model.region
                 , clientInfo = model.clientInfo
                 , cmds =
-                    { authConfigureFailed = Cmd.none
-                    , analyticsConfigureFailed = Cmd.none
-                    , recordFailed = Cmd.none
-                    , fetchNewCredentialsFailed = Cmd.none
+                    { authConfigureFailed = always Cmd.none
+                    , analyticsConfigureFailed = always Cmd.none
+                    , recordFailed = always Cmd.none
+                    , fetchNewCredentialsFailed = always Cmd.none
                     }
                 }
                 subMsg
